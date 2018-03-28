6664503af801f622ce4b7de1990a52f872f819d6
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-03-28 14:07:32 +0200

    Handle factorial case where n == 0. Additional tests added.

--- Factorize a Number/factorize-tests.py
+++ Factorize a Number/factorize-tests.py
@@ -9,11 +9,29 @@ class TestFactorize(unittest.TestCase):
         result = factorizer.factorize(5)
         self.assertIsInstance(result, int, "Expected an int, but got" + str(type(result)))
 
-    def test_factorize(self):
+    def test_factorize_5(self):
         factorizer = factorize.Factorize()
         expected = 120
         result = factorizer.factorize(5)
-        self.assertEqual(result, 120, "Expected: " + str(expected) + " but got: " + str(result))
+        self.assertEqual(result, expected, "Expected: " + str(expected) + " but got: " + str(result))
+
+    def test_factorize_10(self):
+        factorizer = factorize.Factorize()
+        expected = 3628800
+        result = factorizer.factorize(10)
+        self.assertEqual(result, expected, "Expected: " + str(expected) + " but got: " + str(result))
+
+    def test_factorize_20(self):
+        factorizer = factorize.Factorize()
+        expected = 2432902008176640000
+        result = factorizer.factorize(20)
+        self.assertEqual(result, expected, "Expected: " + str(expected) + " but got: " + str(result))
+
+    def test_factorize_0(self):
+        factorizer = factorize.Factorize()
+        expected = 1
+        result = factorizer.factorize(0)
+        self.assertEqual(result, expected, "Expected: " + str(expected) + " but got: " + str(result))
 
 
 if __name__ == '__main__':
