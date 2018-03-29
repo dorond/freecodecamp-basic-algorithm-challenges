763d354ebd59d0a21617813a6d45be36d91b6911
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-03-29 10:28:57 +0200

    Palindrone solution complete, all tests pass

--- Factorize a Number/Check for Palindrones/palindrone-tests.py
+++ Factorize a Number/Check for Palindrones/palindrone-tests.py
@@ -28,6 +28,34 @@ class TestPalindrone(unittest.TestCase):
         result = palindrone.checkPalindrone("A man, a plan, a canal. Panama")
         self.assertTrue(result, "Expected True but got False")
 
+    def test_palindrone_check_long_sentence_with_spaces_should_return_true(self):        
+        result = palindrone.checkPalindrone("never odd or even")
+        self.assertTrue(result, "Expected True but got False")
+
+    def test_palindrone_single_word_non_palindrone_should_return_false(self):        
+        result = palindrone.checkPalindrone("nope")
+        self.assertFalse(result, "Expected False but got True")
+
+    def test_palindrone_single_word_almost_palindrone_should_return_false(self):        
+        result = palindrone.checkPalindrone("almostomla")
+        self.assertFalse(result, "Expected False but got True")
+
+    def test_palindrone_check_mix_punctuation_numbers_letters_spaces_should_return_true(self):        
+        result = palindrone.checkPalindrone("My age is 0, 0 si ega ym.")
+        self.assertTrue(result, "Expected True but got False")
+
+    def test_palindrone_mixing_words_and_numbers_should_return_false(self):        
+        result = palindrone.checkPalindrone("1 eye for of 1 eye.")
+        self.assertFalse(result, "Expected False but got True")
+
+    def test_palindrone_check_lots_of_synbols_and_numbers_should_return_true(self):        
+        result = palindrone.checkPalindrone("0_0 (: /-\ :) 0-0")
+        self.assertTrue(result, "Expected True but got False")
+
+    def test_palindrone_mixing_words_and_symbols_should_return_false(self):        
+        result = palindrone.checkPalindrone("five|\_/|four")
+        self.assertFalse(result, "Expected False but got True")
+
 if __name__ == '__main__':
     # Run the tests and print verbose output to stderr.
     suite = unittest.TestSuite()
--- Factorize a Number/Check for Palindrones/palindrone.py
+++ Factorize a Number/Check for Palindrones/palindrone.py
@@ -9,6 +9,7 @@
 # We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
 import string
 
+
 def checkPalindrone(s):
     original = s.strip().lower()
     stripped = ""
