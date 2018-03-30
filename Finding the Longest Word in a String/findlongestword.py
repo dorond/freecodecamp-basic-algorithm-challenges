50c3518772cb1b33f36fe86fb9ea9d01c617eb73
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-03-30 15:16:46 +0200

    Simplied function using list comprehensions

--- Finding the Longest Word in a String/findlongestword.py
+++ Finding the Longest Word in a String/findlongestword.py
@@ -1,10 +1,5 @@
 # Return the length of the longest word in the provided sentence.
 # Your response should be a number.
 
-def findHowLong(text):
-    result = 0
-    words = text.split()
-    for word in words:
-        if len(word) > result:
-            result = len(word)
-    return result
+def findHowLong(text):     
+    return max([len(word) for word in text.split()])
