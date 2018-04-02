791d2ba05ce0be8eb9fe799a8d3d824b59b3b766
Doron Dusheiko <doron.dusheiko@gmail.com>
2018-04-02 19:00:34 +0200

    Provided a more general solution

--- Reverse a String/stringreverse.py
+++ Reverse a String/stringreverse.py
@@ -4,6 +4,16 @@ class StringReverser(object):
         pass
 
     def reverse(self, s):
-        return s[::-1]
+        # The easy Pythonic solution
+        #return s[::-1]
+
+        # A more general solution
+        reversed = ''
+        count = len(s)
+
+        while count > 0:
+            reversed += s[count - 1]
+            count -= 1
+        return reversed
 
     
